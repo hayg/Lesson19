a5b3e13c1ba52fa29812743371ace206ae3205aa
GBTC441017UR <GBTC441017UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-03-16 14:21:28 -0400

    Initial commit

---
+++ src/test/java/com/example/demo/SpringBoot19AApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot19AApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
